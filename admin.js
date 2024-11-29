d036d403767eb7ef4f51ecc0a54f8341167b4e06
Ex-007 <agbebibidemi02@gmail.com>
2024-11-29 10:39:56 +0100

    Added supabase

--- admin.js
+++ admin.js
@@ -15,7 +15,12 @@ const supabaseKey =
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
-console.log(supabase);
+// console.log(supabase);
+
+// const user = supabase.auth.user();
+// if (!user) {
+//   console.error('User is not authenticated');
+// }
 
 
 // THE INPUTS
@@ -25,19 +30,31 @@ let blogContentIn = document.getElementById('blogContent')
 let blogImageIn = document.getElementById('blogImage')
 
 // THE BUTTONS
-let writeBlog = document.getElementById('writeBlog')
-let readBlog = document.getElementById('readBlog')
-let updateBlog = document.getElementById('updateBlog')
-let deleteBlog = document.getElementById('deleteBlog')
+let writeBlogBtn = document.getElementById('writeBlog')
+let readBlogBtn = document.getElementById('readBlog')
+let updateBlogBtn = document.getElementById('updateBlog')
+let deleteBlogBtn = document.getElementById('deleteBlog')
 
 
 
 
 // WRITING TO THE DATABASE
-const writeBlogFunct = async () => {
+const writeBlog = async () => {
     let blogTitle = blogTitleIn.value
     let blogAuthor = blogAuthorIn.value
     let blogContent = blogContentIn.value
+
+    // SLUGIFY THE TITLE
+    function stringify(blogTitle){
+        return blogTitle.toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .split('')
+        .map(character => (/[a-z0-9]/.test(character) ? character : '-'))
+        .join('')
+        .replace(/-+/g, '-')
+        .replace(/^- |- $/g, '')
+    }
 
     if(blogTitle === '' || blogAuthor === '' || blogContent === ''){
         alert('No field should be left empty')
@@ -45,43 +62,178 @@ const writeBlogFunct = async () => {
     }else{
 
         let file = blogImageIn.files[0]
+        console.log(file)
         var fileName = file.name
         const filePath = `BLOG/${fileName}`
         const{data:uploadData, error:uploadError} = await supabase.storage
-        .from('Paean')
+        .from('paean')
         .upload(filePath, file)
         if(uploadError){
-            console.error('Upload Error: ', uploadError)
+            console.error('Upload Error: ', uploadError.message)
             return;
         }
         console.log('File Successfully Uploaded:', uploadData);
 
         // GETTING BACK THE UPLOAD URL
         const {data:publicUrlData} = await supabase.storage
-        .from('Paean')
+        .from('paean')
         .getPublicUrl(filePath)
         const fileUrl = publicUrlData.publicUrl
         console.log('public url:', fileUrl);
         
         // SAVE THE DETAILS TO THE DATABASE
         const{data:dbData, error:dbError} = await supabase
-        .from('PAEAN')
+        .from('Paean')
         .insert([
             {
                 blogContent : blogContent,
                 blogImage : fileUrl,
                 blogTitle : blogTitle,
                 blogAuthor : blogAuthor,
-                Slug : blogSlug
+                Slug : stringify(blogTitle)
             }
         ])
         if(dbError){
-            console.error('Database Error:', dbError)
+            alert('Database Error:', dbError)
         }else{
-            console.log('Post Successful:', dbData)
+            alert('Post Successful:', dbData)
         }
         
     }
+    blogTitleIn.value = ''
+    blogAuthorIn.value = ''
+    blogContentIn.value = ''
 }
 
-writeBlog.addEventListener('click', writeBlogFunct)
+writeBlogBtn.addEventListener('click', writeBlog)
+
+
+
+// READING FROM THE DATABASE
+const readBlog = async () => {
+    let blogging = blogTitleIn.value
+    const {data, error} = await supabase
+    .from('Paean')
+    .select()
+    .eq('blogTitle', blogging)
+    if(error){
+        console.error(error)
+    }else{
+        console.log(data);
+    }
+}
+
+readBlogBtn.addEventListener('click', readBlog)
+
+
+
+// UPDATING THE BLOG
+const updateBlog = async () => {
+    let blogTitle = blogTitleIn.value
+    let blogAuthor = blogAuthorIn.value
+    let blogContent = blogContentIn.value
+
+        // SLUGIFY THE TITLE
+        function stringify(blogTitle){
+            return blogTitle.toLowerCase()
+            .normalize('NFD')
+            .replace(/[\u0300-\u036f]/g, '')
+            .split('')
+            .map(character => (/[a-z0-9]/.test(character) ? character : '-'))
+            .join('')
+            .replace(/-+/g, '-')
+            .replace(/^- |- $/g, '')
+        }
+
+    if(blogTitle === '' || blogAuthor === '' || blogContent === ''){
+        alert('fill all empty spaces please')
+    }else{
+        const {data, error} = await supabase
+        .from('Paean')
+        .update([
+            {
+                blogContent : blogContent,
+                blogTitle : blogTitle,
+                blogAuthor : blogAuthor,
+                Slug : stringify(blogTitle)
+            }
+        ])
+        .eq('blogTitle', blogTitle)
+        if(error){
+            alert('Updating error:', error.message)
+        }else{
+            alert('Document Updated');
+            
+        }
+
+    }
+}
+
+updateBlogBtn.addEventListener('click', updateBlog)
+
+// DELETING THE BLOG
+
+const deleteBlog = async () => {
+    let blogTitle = blogTitleIn.value
+
+    const{data, error} = await supabase
+    .from('Paean')
+    .delete()
+    .eq('blogTitle', blogTitle)
+    
+    if(error){
+        alert('Error Deleting:', error.message)
+    }else{
+        alert('Document Deleted')
+        // console.log(data)
+    }
+}
+
+deleteBlogBtn.addEventListener('click', deleteBlog)
+
+
+let mainDisplay = document.getElementById('allContents')
+// FETCH AND DISPLAY ALL CONTENTS
+const fetchAll = async () => {
+    const {data, error} = await supabase
+    .from('Paean')
+    .select()
+    if(error){
+        console.error(error)
+    }else{
+        data.forEach(element => {
+            let blogImageFetched = element.blogImage
+            let blogTitleFetched = element.blogTitle
+            let slugFetched = element.Slug
+            let blogContentFetched = element.blogContent
+            let blogAuthorFetched = element.blogAuthor
+            // let blogDateFetched = element.blogDate
+
+
+            let newDiv = document.createElement('div')
+            newDiv.setAttribute('class', 'postedBlog')
+            newDiv.innerHTML = `
+                <div class="imgTab">
+                    <img src="${blogImageFetched}" alt="avatar">
+                </div>
+                <div class="others">
+                    <h3>14th November, 2024</h3>
+                    <h3>${blogAuthorFetched}</h3>
+                    <h3>${blogTitleFetched}</h3>
+                    <p>${blogContentFetched} </p>
+                </div>
+                <button>Share</button>
+            `
+            mainDisplay.appendChild(newDiv)
+            // console.log(element);
+            
+        });
+        // console.log(data);
+    }
+}
+
+fetchAll()
+
+
+https://opjxgfjfjvcexhizvyar.supabase.co/storage/v1/object/public/paean/BLOG/avatar-1.jpg
+"https://opjxgfjfjvcexhizvyar.supabase.co/storage/v1/object/public/Paean/BLOG/avatar-3.jpg"
